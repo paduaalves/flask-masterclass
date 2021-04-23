9803ecb62920206648c5c3e71aac749150feb261
Pádua Alves <paduaalves03@gmail.com>
2021-04-23 19:27:16 -0300

    Manipulando requisições

--- introducao/app.py
+++ introducao/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Página Inicial"
+    return "<a href='/posts'>Posts</a>"
+
+
+@app.route("/posts")
+@app.route("/posts/<int:id>")
+def posts(id):
+    data = dict(
+        patth=request.path,
+        referrer=request.referrer,
+        content_type=request.content_type,
+        method=request.method,
+        titulo=request.args.get("titulo"),
+        id=id if id else 0
+
+    )
+    return data
+
 
 if __name__ == "__main__":
     app.run()
